dfa8921a3003ba9852c897845e63f67cfa3bad92
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 15:34:43 -0700

    Updated with While Loop

--- game.py
+++ game.py
@@ -13,12 +13,36 @@ from random import randint
 name = raw_input("What's your name? ")
 number = randint(1,100)
 list_guess = []
+guess = None
 
 print "Hi, %s! Let's play a guessing game." % name 
 print "Don't tell anyone, but the number is " + str(number)
-while True:
-    guess = int(raw_input("Guess a number between 1 and 100: " ))    
-    print guess
+print "Shhh...The number is %d" % number
+
+# This is the while loop - in progress!
+while guess != number:
+    guess = int(raw_input("Guess a number between 1 and 100: " ))
+
+    if guess in list_guess:
+        print "You've already guessed that, try again!"
+
+    elif 1 > guess  or guess > 100 :
+        print "You're way out of range! What are you smoking?"
+        list_guess.append(guess)
+
+    elif guess > number:
+        print "Too high, guess again."
+        list_guess.append(guess)
+
+    elif guess < number:
+        print 'Your guess is too low, try again!'
+        list_guess.append(guess)
+
+if guess == number:
+    list_guess.append(guess)
+    print "Congratulations! You've guessed the number! It took you %d guesses." % len(list_guess)
+
+
 #this is the recursive method
 
 # def game():
